c604471de2bf1943224465971768e73317168428
vpei <vpei@live.com>
2021-12-26 21:48:49 +0800

    Update config for NodeSpeed

---
+++ SSRSpeedExample.bat
@@ -0,0 +1,3 @@
+python ./main.py -u "http://121.36.203.35:8080/ipns/k51qzi5uqu5dlfnig6lej7l7aes2d5oed6a4435s08ccftne1hq09ac1bulz2f/nodecn.txt"
+start explorer "保存网速测试截图的地址"
+pause
---
+++ 打开目录.bat
@@ -0,0 +1,1 @@
+start explorer "C:\SSRSpeed\results"
